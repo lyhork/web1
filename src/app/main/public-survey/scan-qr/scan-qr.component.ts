761f9d5061acf9c200ce9a4c818089b9b93f8fd2
lyhork <lyhorklim@gmail.com>
2024-01-03 15:43:24 +0700

    wording

--- src/app/main/public-survey/scan-qr/scan-qr.component.ts
+++ src/app/main/public-survey/scan-qr/scan-qr.component.ts
@@ -1,49 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PublicSurveyService } from '../public-survey.service';
-import { environment as env } from 'environments/environment';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { PublicSurveyService } from "../public-survey.service";
+import { environment as env } from "environments/environment";
 @Component({
-    selector: 'app-scan-qr',
-    templateUrl: './scan-qr.component.html',
-    styleUrls: ['./scan-qr.component.scss']
+  selector: "app-scan-qr",
+  templateUrl: "./scan-qr.component.html",
+  styleUrls: ["./scan-qr.component.scss"],
 })
 export class ScanQrComponent implements OnInit {
+  public myAngularxQrCode: string;
+  countdown: number = 30;
+  interval: any; // Declare interval variable
+  public req_id: number = 0;
+  public type_id: number = 0;
+  public id: number = 0;
+  public data: any[] = [];
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private publicService: PublicSurveyService
+  ) {
+    this.publicService.getSurvey().subscribe((res: any) => {
+      this.data = res;
+      this.id = this.data["id"];
+      this.myAngularxQrCode = env.webUrl + "public/survey/" + this.id;
+      if (!localStorage.getItem("foo")) {
+        localStorage.setItem("foo", "no reload");
+        location.reload();
+      } else {
+        localStorage.removeItem("foo");
+      }
+    });
+    this._route.paramMap.subscribe((params: any) => {
+      this.req_id = params.get("id");
+    });
+  }
 
-    public myAngularxQrCode: string;
-    countdown: number = 30;
-    interval: any; // Declare interval variable
-    public req_id: number = 0;
-    public type_id: number = 0;
-    public id: number = 0;
-    public data: any[] = [];
-    constructor(
-        private _route: ActivatedRoute,
-        private _router: Router,
-        private publicService: PublicSurveyService
-    ) {
-        this.publicService.getSurvey().subscribe((res: any) => {
-            
-            this.data = res;
-            this.id = this.data['id']
-            this.myAngularxQrCode = env.webUrl+'public/survey/'+this.id;
-            
-        });
-        this._route.paramMap.subscribe((params: any) => {
-            this.req_id = params.get('id');
-        });
-    }
-
-    ngOnInit(): void {
-        this.interval = setInterval(() => {
-            this.countdown--;
-            if (this.countdown === -1) {
-                clearInterval(this.interval); // Use clearInterval with interval variable
-                this._router.navigate(['/thank']);
-            }
-        }, 1000);
-    }
-    back(): void {
+  ngOnInit(): void {
+    this.interval = setInterval(() => {
+      this.countdown--;
+      if (this.countdown === -1) {
         clearInterval(this.interval); // Use clearInterval with interval variable
-        this._router.navigateByUrl('/public/reg/'+this.req_id);
-    }
+        this._router.navigate(["/thank"]);
+      }
+    }, 1000);
+  }
+  back(): void {
+    clearInterval(this.interval); // Use clearInterval with interval variable
+    this._router.navigateByUrl("/public/reg/" + this.req_id);
+  }
 }
